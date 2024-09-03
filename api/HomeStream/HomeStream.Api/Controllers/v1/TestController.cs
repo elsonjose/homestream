4afedf1294191d4b4e43d0881bf71777cbd75b49
Elson Jose <elsonjoseofficial@gmail.com>
2024-09-03 23:53:24 +0530

    API Update - Renamed Test Endpoint - Updated the endpoint and request name

--- api/HomeStream/HomeStream.Api/Controllers/v1/TestController.cs
+++ api/HomeStream/HomeStream.Api/Controllers/v1/TestController.cs
@@ -3,16 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HomeStream.Api.Controllers.v1;
 
+/// <summary>
+/// Defines the controller for testing the status of the application.
+/// </summary>
 public class TestController : BaseController
 {
     /// <summary>
     /// Gets the addition result for testing the code.
     /// </summary>
-    /// <returns></returns>
+    /// <param name="leftOperand">The left operand</param>
+    /// <param name="rightOperand">The right operand</param>
+    /// <returns>The result of addition operation.</returns>
     [HttpGet("test")]
-    public async Task<IActionResult> GetTestResult([FromQuery] int leftOperand, [FromQuery] int rightOperand)
+    public async Task<IActionResult> AddOperation([FromQuery] int leftOperand, [FromQuery] int rightOperand)
     {
-        return await Execute(new TestQuery()
+        return await Execute(new AddQuery()
         {
             LeftOperand = leftOperand,
             RightOperand = rightOperand
--- api/HomeStream/HomeStream.Application/Request/AddQuery.cs
+++ api/HomeStream/HomeStream.Application/Request/AddQuery.cs
@@ -3,15 +3,15 @@ using MediatR;
 
 namespace HomeStream.Application.Request;
 
-public class TestQuery : IRequest<ResponseDto<int>>
+public class AddQuery : IRequest<ResponseDto<int>>
 {
     public int LeftOperand {  get; set; }
     public int RightOperand {  get; set; }
 }
 
-public class TestQueryHandler : IRequestHandler<TestQuery, ResponseDto<int>>
+public class AddQueryHandler : IRequestHandler<AddQuery, ResponseDto<int>>
 {
-    public async Task<ResponseDto<int>> Handle(TestQuery request, CancellationToken cancellationToken)
+    public async Task<ResponseDto<int>> Handle(AddQuery request, CancellationToken cancellationToken)
     {
         return await Task.FromResult(new ResponseDto<int>(request.LeftOperand + request.RightOperand));
     }
