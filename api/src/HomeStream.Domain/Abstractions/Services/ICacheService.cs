1090ddd58eec332b1614c378feff51a00bdc5c09
Elson Jose <elsonjoseofficial@gmail.com>
2024-09-09 21:12:11 +0530

    API Update - Redis Cache Implementation Modified - Documentation added - Modified log content for cache operations

--- api/src/HomeStream.Domain/Abstractions/Services/ICacheService.cs
+++ api/src/HomeStream.Domain/Abstractions/Services/ICacheService.cs
@@ -1,12 +1,41 @@
 ﻿namespace HomeStream.Domain.Abstractions.Services;
 
+/// <summary>
+/// Defines the cache service.
+/// </summary>
 public interface ICacheService
 {
+    /// <summary>
+    /// Sets the cache item
+    /// </summary>
+    /// <typeparam name="ICacheType">The type of item</typeparam>
+    /// <param name="key">The cache key</param>
+    /// <param name="value">The item value</param>
+    /// <param name="expiryInSeconds">The expiration in seconds</param>
+    /// <returns>True if cache is set, else false.</returns>
     public Task<bool> SetCacheItemAsync<ICacheType>(string key, ICacheType value, int? expiryInSeconds = null);
 
+    /// <summary>
+    /// Gets the cached item.
+    /// </summary>
+    /// <typeparam name="ICacheType">The type of item</typeparam>
+    /// <param name="key">The cache key</param>
+    /// <returns>The cache item of type <seealso cref="ICacheType"/></returns>
     public Task<ICacheType?> GetCachedItemAsync<ICacheType>(string key);
 
+    /// <summary>
+    /// Removes the cached item.
+    /// </summary>
+    /// <param name="key">The cache key</param>
+    /// <returns>True if cache item is removed, else false.</returns>
     public Task<bool> RemoveCachedItemAsync(string key);
 
+    /// <summary>
+    /// Reloads the cache item by calling <seealso cref="RemoveCachedItemAsync(string)"/> and <seealso cref="SetCacheItemAsync{ICacheType}(string, ICacheType, int?)"/>
+    /// <typeparam name="ICacheType">The type of item</typeparam>
+    /// <param name="key">The cache key</param>
+    /// <param name="newValue">The new item value</param>
+    /// <param name="expiryInSeconds">The expiration in seconds</param>
+    /// <returns>True if cache is set, else false.</returns>
     public Task<bool> ReloadCachedItemAsync<ICacheType>(string key, ICacheType newValue, int? expiryInSeconds = null);
 }
--- api/src/Homestream.Infrastructure/Implementations/RedisCacheService.cs
+++ api/src/Homestream.Infrastructure/Implementations/RedisCacheService.cs
@@ -5,44 +5,64 @@ using StackExchange.Redis;
 
 namespace HomeStream.Infrastructure.Implementations;
 
+/// <summary>
+/// Defines the <seealso cref="ICacheService"/> implementation for redis-server.
+/// </summary>
 public class RedisCacheService : ICacheService
 {
+    /// <inheritdoc/>
     private readonly IDatabase _redisDb;
+
+    /// <inheritdoc/>
     private readonly ILogger<RedisCacheService> _logger;
 
+    /// <summary>
+    /// Initializes a new instance of <seealso cref="RedisCacheService"/>
+    /// </summary>
+    /// <param name="redis">The redis multiplexer API</param>
+    /// <param name="logger">The logger instance</param>
     public RedisCacheService(IConnectionMultiplexer redis, ILogger<RedisCacheService> logger)
     {
         _redisDb = redis.GetDatabase();
         _logger = logger;
     }
 
+    /// <inheritdoc/>
     public async Task<ICacheType?> GetCachedItemAsync<ICacheType>(string key)
     {
         var rediCacheItem = await _redisDb.StringGetAsync(key);
-        _logger.LogInformation("Getting cache item for key: {key} with value:{value}", key, rediCacheItem);
+        _logger.LogInformation("Getting cache item for key: {key} with value: {value}", key, rediCacheItem);
         return rediCacheItem.HasValue ? JsonConvert.DeserializeObject<ICacheType>(rediCacheItem.ToString()) : default;
     }
 
+    /// <inheritdoc/>
     public async Task<bool> ReloadCachedItemAsync<ICacheType>(string key, ICacheType newValue, int? expiryInSeconds = null)
     {
         await RemoveCachedItemAsync(key);
         return await SetCacheItemAsync(key, newValue, expiryInSeconds);
     }
 
+    /// <inheritdoc/>
     public async Task<bool> RemoveCachedItemAsync(string key)
     {
         _logger.LogInformation("Removing cache item for key: {key}", key);
         return await _redisDb.KeyDeleteAsync(key);
     }
 
+    /// <inheritdoc/>
     public async Task<bool> SetCacheItemAsync<ICacheType>(string key, ICacheType value, int? expiryInSeconds = null)
     {
         await RemoveCachedItemAsync(key);
         var serializedCache = JsonConvert.SerializeObject(value);
-        _logger.LogInformation("Setting cache item for key: {key} with value:{value}", key, value);
+        _logger.LogInformation("Setting cache item for key: {key} with value: {value}", key, value);
         return await _redisDb.StringSetAsync(key, serializedCache, GetExpiryInSeconds(expiryInSeconds));
     }
 
+    /// <summary>
+    /// Gets the expiry in seconds.
+    /// </summary>
+    /// <param name="expiryInSeconds">The expiry in seconds</param>
+    /// <returns>Timespan equivalent of seconds to expire if valid expiryInSeconds argument else null.</returns>
     private static TimeSpan? GetExpiryInSeconds(int? expiryInSeconds)
     {
         return expiryInSeconds.HasValue ? TimeSpan.FromSeconds(expiryInSeconds.Value) : null;
